62c712a55d88aed7522819e4f0ce9f3185d5109c
cmarker <cmarker4@gmail.com>
2020-11-30 20:41:34 -0600

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,33 @@
+
+class Artist
+    attr_accessor :name, :song, :genre
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @songs = []
+    end
+
+    def songs
+        Song.all.collect { |song| song if song.artist == self }
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.destroy_all
+        @@all.clear
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.create(name)
+        artist = Artist.new(name)
+        artist.save
+        artist
+    end
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,30 @@
+
+
+class Song
+    attr_accessor :name, :artist, :genre
+    @@all = []
+
+    def initialize(name, artist = nil)
+        @name = name
+        @artist = artist
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.destroy_all
+        @@all.clear
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.create(name)
+        song = Song.new(name)
+        song.save
+        self
+    end
+
+end
